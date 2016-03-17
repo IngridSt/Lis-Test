c0886ceef1d1d72b47296acf09f5db6986cc63f7
Ingrid Stoleru <ingridstoleru@gmail.com>
2016-03-17 15:52:27 +0200

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -245,7 +245,7 @@ copy_bash C:\Users\Administrator\Desktop\Script_Ingrid C:\Users\Administrator\De
 function Copiere_Bash_Script
 {
  param([int]$ipv, [string]$PathofScript )
- C:\Users\Administrator\Desktop\Script_Ingrid\pscp.exe -q -i ssh\${sshKey} root@${ipv}:${PathofScript} .
+ C:\Users\Administrator\Desktop\Script_Ingrid\pscp.exe -q -i ssh\${sshKey} root@${ipv}:${PathofScript} 
 }
 
 
